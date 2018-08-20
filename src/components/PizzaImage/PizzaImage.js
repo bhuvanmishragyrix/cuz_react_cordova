a9039a50f90b6aa00983bc6b093c1acd5ead98e7
Bhuvan Mishra <bhuvan.mishra@gyrix.co>
2018-08-20 15:15:30 +0530

    Content security policy set for image.

--- src/components/PizzaImage/PizzaImage.js
+++ src/components/PizzaImage/PizzaImage.js
@@ -64,13 +64,13 @@ class PizzaImageClass extends Component {
                 <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
                     <div className="carousel-inner">
                         <div className="carousel-item active">
-                            <img className="d-block w-100" src={PizzaImage} alt="First slide" />
+                            <img className="d-block w-100" src={`https://s3-ap-southeast-2.amazonaws.com/cuz-gyrix/static/media/logo.5d5d9eef.svg`} alt="First slide" />
                         </div>
                         <div className="carousel-item">
-                            <img className="d-block w-100" src={PizzaImage} alt="Second slide" />
+                            <img className="d-block w-100" src={`https://s3-ap-southeast-2.amazonaws.com/cuz-gyrix/static/media/logo.5d5d9eef.svg`} alt="Second slide" />
                         </div>
                         <div className="carousel-item">
-                            <img className="d-block w-100" src={PizzaImage} alt="Third slide" />
+                            <img className="d-block w-100" src={`https://s3-ap-southeast-2.amazonaws.com/cuz-gyrix/static/media/logo.5d5d9eef.svg`} alt="Third slide" />
                         </div>
                     </div>
                     <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">

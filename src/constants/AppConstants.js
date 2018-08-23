9ada222193110d84ca852292e1ced03513dc9710
Bhuvan Mishra <bhuvan.mishra@gyrix.co>
2018-08-23 12:31:35 +0530

    Added link to fetch JSON data from in app constants file.

--- src/constants/AppConstants.js
+++ src/constants/AppConstants.js
@@ -1,1 +1,3 @@
 export const HeightToSubtractFromWindowScreenHeight = 20;
+
+export const LINK_TO_FETCH_JSON_DATA_FROM = "https://s3-ap-southeast-2.amazonaws.com/cuz-gyrix/CuzProductsAndImagesData.json";

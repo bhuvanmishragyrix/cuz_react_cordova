8c0d2406b68e20ec0bb509c4dfdb05da0d885fe0
Bhuvan Mishra <bhuvan.mishra@gyrix.co>
2018-08-23 17:42:06 +0530

    Restructured Parent and startpage components. Resolved routing issue.

--- src/containers/ParentForThreeElementTabBarScreens.js
+++ src/containers/ParentForThreeElementTabBarScreens.js
@@ -1,51 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import { connect } from 'react-redux';
 
 import ThreeElementTabBar from '../components/ThreeElementTabBar/ThreeElementTabBar';
 import CategorySelectPage from '../containers/CategorySelectPage';
-import { getFromAPI } from '../util/Util';
-import { LINK_TO_FETCH_JSON_DATA_FROM } from '../constants/AppConstants';
-import { STORE_FETCHED_PRODUCTS_AND_IMAGES_JSON_DATA } from '../store/actionTypes';
 
-class ParentForThreeElementTabBarScreens extends Component {
+const parentForThreeElementTabBarScreens = () => {
+    return (
+        <div>
+            <ThreeElementTabBar />
+            <Switch>
+                <Route path="/parentForThreeElementTabBarScreens/categorySelectPage" component={CategorySelectPage} />
+            </Switch>
+        </div>
+    );
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            jSONData: null
-        };
-    }
-
-    componentDidMount() {
-        getFromAPI(LINK_TO_FETCH_JSON_DATA_FROM)
-            .then((data) => {
-                this.setState({
-                    jSONData: data.data
-                });
-                this.props.storeProductsAndImagesJSONData(data.data);
-            })
-            .catch((err) => { console.log("Error", err) });
-    }
-
-    render() {
-        return (
-            <div>
-                <ThreeElementTabBar />
-                <Switch>
-                    <Route path="/parentForThreeElementTabBarScreens/categorySelectPage" component={CategorySelectPage} productsAndImagesJSONData={this.state.jSONData} />
-                </Switch>
-            </div>
-        );
-    }
 };
 
-const mapDispatchToProps = (dispatch) => {
-    return {
-        storeProductsAndImagesJSONData: (data) => {
-            dispatch({ type: STORE_FETCHED_PRODUCTS_AND_IMAGES_JSON_DATA, payload: data })
-        }
-    };
-};
-
-export default connect(null, mapDispatchToProps)(ParentForThreeElementTabBarScreens);
+export default parentForThreeElementTabBarScreens;
--- src/containers/StartPage.js
+++ src/containers/StartPage.js
@@ -1,18 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 
 import LandingPage from './LandingPage';
 import ParentForThreeElementTabBarScreens from './ParentForThreeElementTabBarScreens';
+import { getFromAPI } from '../util/Util';
+import { LINK_TO_FETCH_JSON_DATA_FROM } from '../constants/AppConstants';
+import { STORE_FETCHED_PRODUCTS_AND_IMAGES_JSON_DATA } from '../store/actionTypes';
 
-const StartPage = () => {
-    return (
-        <div>
-            <Switch>
-                <Route path="/parentForThreeElementTabBarScreens" component={ParentForThreeElementTabBarScreens}/>
-                <Route render={LandingPage} />
-            </Switch>
-        </div>
-    );
+class StartPage extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+        getFromAPI(LINK_TO_FETCH_JSON_DATA_FROM)
+            .then((data) => {
+                this.setState({
+                    jSONData: data.data
+                });
+                this.props.storeProductsAndImagesJSONData(data.data);
+            })
+            .catch((err) => { console.log("Error", err) });
+    }
+
+    render() {
+        return (
+            <div>
+                <Switch>
+                    <Route path="/parentForThreeElementTabBarScreens" component={ParentForThreeElementTabBarScreens} />
+                    <Route render={LandingPage} />
+                </Switch>
+            </div>
+        );
+    }
 };
 
-export default StartPage;
+const mapDispatchToProps = (dispatch) => {
+    return {
+        storeProductsAndImagesJSONData: (data) => {
+            dispatch({ type: STORE_FETCHED_PRODUCTS_AND_IMAGES_JSON_DATA, payload: data })
+        }
+    };
+};
+
+export default withRouter(connect(null, mapDispatchToProps)(StartPage));

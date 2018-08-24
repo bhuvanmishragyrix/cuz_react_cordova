f614c98d89c5a1e8ca6f465d4dc64cc8aacf7037
Bhuvan Mishra <bhuvan.mishra@gyrix.co>
2018-08-24 11:43:31 +0530

    Changed height of threeElementTabBar to be taken from app constants.

--- src/components/ThreeElementTabBar/ThreeElementTabBar.js
+++ src/components/ThreeElementTabBar/ThreeElementTabBar.js
@@ -8,17 +8,13 @@ import * as appConstants from '../../constants/AppConstants';
 
 const threeElementTabBar = () => {
 
-    let logoHeight;
-
-    logoHeight = appConstants.HEIGHT_OF_THREE_ELEMENT_TAB_BAR;
-
     // console.log($(`.threeElementTabBarRoot`)[0].outerHTML);
     
     return (
         <div className="threeElementTabBarRoot p-3 d-flex align-items-center justify-content-center">
-            <i style={{ fontSize: `${logoHeight}px` }} className={`${ThreeElementTabBarStyles.tabBarIconColor} ${ThreeElementTabBarStyles.hamburgerMenu} fa fa-bars`} aria-hidden="true"></i>
-            <img src={logo} style={{ height: `${logoHeight}px` }} />
-            <i style={{ fontSize: `${logoHeight}px` }} className={`${ThreeElementTabBarStyles.tabBarIconColor} ${ThreeElementTabBarStyles.shareMenu} fa fa-share-alt`} aria-hidden="true"></i>
+            <i style={{ fontSize: `${appConstants.HEIGHT_OF_THREE_ELEMENT_TAB_BAR}px` }} className={`${ThreeElementTabBarStyles.tabBarIconColor} ${ThreeElementTabBarStyles.hamburgerMenu} fa fa-bars`} aria-hidden="true"></i>
+            <img src={logo} style={{ height: `${appConstants.HEIGHT_OF_THREE_ELEMENT_TAB_BAR}px` }} />
+            <i style={{ fontSize: `${appConstants.HEIGHT_OF_THREE_ELEMENT_TAB_BAR}px` }} className={`${ThreeElementTabBarStyles.tabBarIconColor} ${ThreeElementTabBarStyles.shareMenu} fa fa-share-alt`} aria-hidden="true"></i>
         </div>
     );
 

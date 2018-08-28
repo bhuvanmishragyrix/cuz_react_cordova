df8a1fa01f732cbdc99124f1f372014c21334a9a
Bhuvan Mishra <bhuvan.mishra@gyrix.co>
2018-08-28 14:58:00 +0530

    Stopped autoslide of category carousel.

--- src/components/CategorySelect/CategoryCarouselComponent/CarouselComponent.js
+++ src/components/CategorySelect/CategoryCarouselComponent/CarouselComponent.js
@@ -29,16 +29,16 @@ const carouselComponent = (props) => {
         })
 
         content = (
-            <div id="carouselExampleControls" className="carousel slide" data-ride="false">
+            <div id="carouselExampleControls" className="carousel slide" data-interval="false" data-ride="carousel">
                 <div className="carousel-inner">
                     {carouselImageAndCaptionContent}
                 </div>
-                <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                <a className={`carousel-control-prev`} href="#carouselExampleControls" role="button" data-slide="prev">
+                    <span className={`carousel-control-prev-icon ${CarouselStyles.setCarouselIndicatorColor}`} aria-hidden="true"></span>
                     <span className="sr-only">Previous</span>
                 </a>
-                <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                <a className={`carousel-control-next`} href="#carouselExampleControls" role="button" data-slide="next">
+                    <span className={`carousel-control-next-icon ${CarouselStyles.setCarouselIndicatorColor}`} aria-hidden="true"></span>
                     <span className="sr-only">Next</span>
                 </a>
             </div>

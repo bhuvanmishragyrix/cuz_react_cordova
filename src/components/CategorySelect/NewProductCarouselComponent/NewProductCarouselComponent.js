87b0ac2c370ad9813045ee04b6ec0bbd656ae728
Bhuvan Mishra <bhuvan.mishra@gyrix.co>
2018-08-28 15:02:29 +0530

    Changed background color of new products carousel.

--- src/components/CategorySelect/NewProductCarouselComponent/NewProductCarouselComponent.js
+++ src/components/CategorySelect/NewProductCarouselComponent/NewProductCarouselComponent.js
@@ -30,11 +30,11 @@ const carouselComponent = (props) => {
                     {carouselImageContent}
                 </div>
                 <a className="carousel-control-prev" href="#newProductCarouselExampleControls" role="button" data-slide="prev">
-                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className={`carousel-control-prev-icon ${CarouselStyles.setCarouselIndicatorColor}`} aria-hidden="true"></span>
                     <span className="sr-only">Previous</span>
                 </a>
                 <a className="carousel-control-next" href="#newProductCarouselExampleControls" role="button" data-slide="next">
-                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className={`carousel-control-next-icon ${CarouselStyles.setCarouselIndicatorColor}`} aria-hidden="true"></span>
                     <span className="sr-only">Next</span>
                 </a>
             </div>

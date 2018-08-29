7656a6af2b308e6f047eef6cb9410878e4fd4acc
Bhuvan Mishra <bhuvan.mishra@gyrix.co>
2018-08-29 10:46:06 +0530

    Converted BrandYearModel carousel to class based component.

--- src/components/BrandYearModelSelection/Carousel/Carousel.js
+++ src/components/BrandYearModelSelection/Carousel/Carousel.js
@@ -1,57 +1,67 @@
-import React from 'react';
+import React, { Component } from 'react';
+import $ from 'jquery';
 
 import { LINK_TO_ROOT_PATH_OF_IMAGES } from '../../../constants/AppConstants';
 import styles from './Carousel.css';
 import { circularProgress } from '../../../util/Util';
 
-const carousel = (props) => {
-    let content, carouselImage;
+class Carousel extends Component {
 
-    if (props.carouselData && props.carouselData.hasOwnProperty('length') && props.carouselData.length > 0) {
-
-
-        carouselImage = props.carouselData.map((el, index) => {
-            let activeClass = "";
-            if (index === 0) {
-                activeClass = "active";
-            }
-
-            return (
-                <div className={`carousel-item ${activeClass} ${styles.setWidthAndHeight}`}>
-                    <div className={`d-flex justify-content-center align-items-center ${styles.fullHeight}`}>
-                        <img className={`${styles.setImageBackgroundSize} w-25`} src={`${LINK_TO_ROOT_PATH_OF_IMAGES}${el.filename}`} alt={el.category} />
-                    </div>
-                </div>
-            )
+    componentDidMount() {
+        $(`#${this.props.carouselId}`).on('slide.bs.carousel', (event) => {
+            this.props.categorySelected(event.to)
         })
+    }
 
-        content = (
-            <div id={props.carouselId} className="carousel slide" data-interval="false" data-ride="carousel">
-                <div className="carousel-inner">
-                    {carouselImage}
+    render() {
+        let content, carouselImage;
+
+        if (this.props.carouselData && this.props.carouselData.hasOwnProperty('length') && this.props.carouselData.length > 0) {
+
+
+            carouselImage = this.props.carouselData.map((el, index) => {
+                let activeClass = "";
+                if (index === 0) {
+                    activeClass = "active";
+                }
+
+                return (
+                    <div className={`carousel-item ${activeClass} ${styles.setWidthAndHeight}`}>
+                        <div className={`d-flex justify-content-center align-items-center ${styles.fullHeight}`}>
+                            <img className={`${styles.setImageBackgroundSize} w-25`} src={`${LINK_TO_ROOT_PATH_OF_IMAGES}${el.filename}`} alt={el.category} />
+                        </div>
+                    </div>
+                )
+            })
+
+            content = (
+                <div id={this.props.carouselId} className="carousel slide" data-interval="false" data-ride="carousel">
+                    <div className="carousel-inner">
+                        {carouselImage}
+                    </div>
+                    <a className={`carousel-control-prev`} href={`#${this.props.carouselId}`} role="button" data-slide="prev">
+                        <span className={`carousel-control-prev-icon ${styles.setCarouselIndicatorColor}`} aria-hidden="true"></span>
+                        <span className="sr-only">Previous</span>
+                    </a>
+                    <a className={`carousel-control-next`} href={`#${this.props.carouselId}`} role="button" data-slide="next">
+                        <span className={`carousel-control-next-icon ${styles.setCarouselIndicatorColor}`} aria-hidden="true"></span>
+                        <span className="sr-only">Next</span>
+                    </a>
                 </div>
-                <a className={`carousel-control-prev`} href={`#${props.carouselId}`} role="button" data-slide="prev">
-                    <span className={`carousel-control-prev-icon ${styles.setCarouselIndicatorColor}`} aria-hidden="true"></span>
-                    <span className="sr-only">Previous</span>
-                </a>
-                <a className={`carousel-control-next`} href={`#${props.carouselId}`} role="button" data-slide="next">
-                    <span className={`carousel-control-next-icon ${styles.setCarouselIndicatorColor}`} aria-hidden="true"></span>
-                    <span className="sr-only">Next</span>
-                </a>
-            </div>
-        );
-    }
-    else {
-        content = (
-            <div className={`${styles.setWidthAndHeight} ${styles.setPlaceHolderBackground} d-flex justify-content-center align-items-center`}>
-                {circularProgress()}
-            </div>
-        );
-    }
+            );
+        }
+        else {
+            content = (
+                <div className={`${styles.setWidthAndHeight} ${styles.setPlaceHolderBackground} d-flex justify-content-center align-items-center`}>
+                    {circularProgress()}
+                </div>
+            );
+        }
 
-    return (
-        content
-    );
+        return (
+            content
+        );
+    }
 };
 
-export default carousel;
+export default Carousel;

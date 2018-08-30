b1d864053d1633ce2b146b17839e3f556d9e483b
Bhuvan Mishra <bhuvan.mishra@gyrix.co>
2018-08-30 17:21:19 +0530

    Added List of graphics logic to project.

--- src/components/GraphicStyleSelect/GraphicStyleSelect.js
+++ src/components/GraphicStyleSelect/GraphicStyleSelect.js
@@ -5,8 +5,19 @@ import styles from './GraphicStyleSelect.css';
 import YourChoiceDisplay from './YourChoiceDisplay/YourChoiceDisplay';
 import CustomizeIt from './CustomizeIt/CustomizeIt';
 import Instructions from './InstructionsText/Instructions';
+import ListOfGraphics from './ListOfGraphics/ListOfGraphics';
+import { connect } from 'react-redux';
 
-const graphicStyleSelect = () => {
+const graphicStyleSelect = (props) => {
+
+    let imageNameData;
+
+    imageNameData = props.images.filter((el) => {
+        if (el.graphic && el.category === props.selectedCategory && el.brand === props.selectedBrand && el.year === props.selectedYear && el.model === props.selectedModel && !el.isBikeSVG) {
+            return el;
+        }
+    });
+
 
     let topMarginGraphicStyleSelect = {
         marginTop: `${appConstants.HEIGHT_OF_THREE_ELEMENT_TAB_BAR + appConstants.TOP_MARGIN_FOR_THREE_ELEMENT_TABBAR_PAGES}px`
@@ -22,8 +33,20 @@ const graphicStyleSelect = () => {
             <div className={`w-100 text-center my-3`}>
                 <p className={`${styles.text}`}>SELECT A STYLE</p>
             </div>
+            <ListOfGraphics imageNameData={imageNameData} />
         </div >
     );
 };
 
-export default graphicStyleSelect;
+const mapStateToProps = (state) => {
+    return {
+        images: state.images,
+        selectedCategory: state.selectedCategory,
+        selectedBrand: state.selectedBrand,
+        selectedYear: state.selectedYear,
+        selectedModel: state.selectedModel,
+
+    };
+};
+
+export default connect(mapStateToProps)(graphicStyleSelect);
---
+++ src/components/GraphicStyleSelect/ListOfGraphics/ListOfGraphics.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import * as appConstants from '../../../constants/AppConstants';
+
+const listOfGraphics = (props) => {
+
+    let content = "";
+
+    if (props.imageNameData) {
+        content = props.imageNameData.map((el) => {
+            return (
+                <img className={`w-100`} src={`${appConstants.LINK_TO_ROOT_PATH_OF_IMAGES}${el.filename}`} />
+            );
+        });
+    }
+
+    return (
+        content
+    );
+};
+
+export default listOfGraphics;

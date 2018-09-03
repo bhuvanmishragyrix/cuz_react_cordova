340b71e0f5b47bb0c9ab7f8d945436b109d9efff
Bhuvan Mishra <bhuvan.mishra@gyrix.co>
2018-09-03 14:55:06 +0530

    Created Skeleton for Visual composer color customiser screen.

--- src/components/VisualComposerColorCustomiser/VisualComposerColorCustomiser.js
+++ src/components/VisualComposerColorCustomiser/VisualComposerColorCustomiser.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
+import * as appContants from '../../constants/AppConstants'
 
 class VisualComposerColorCustomiser extends Component {
+
+    constructor(props) {
+        super(props);
+
+        let remainingHeight = window.screen.height - appContants.HEIGHT_TO_SUBTRACT_FROM_WINDOW_SCREEN_HEIGHT
+
+        this.state = {
+            wrapperDivStyle: {
+                height: `${remainingHeight}px`
+            },
+            controlsDivStyle: {
+                height: `${(15 / 100) * (remainingHeight)}px`
+            },
+            imageDivStyle: {
+                height: `${(85 / 100) * (remainingHeight)}px`
+            }
+        };
+    }
+
     render() {
         return (
-            <div>
-                Visual Composer!
+            <div className={`bg-danger`} style={this.state.wrapperDivStyle}>
+                <div className={`bg-success`} style={this.state.imageDivStyle}></div>
+                <div className={`bg-primary`} style={this.state.controlsDivStyle}>
+                </div>
             </div>
         );
     }

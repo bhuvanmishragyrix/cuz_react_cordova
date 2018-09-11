eeb9009801e14f5f170e4579efdcfc183a9e8553
Bhuvan Mishra <bhuvan.mishra@gyrix.co>
2018-09-11 11:09:36 +0530

    Added color input in bottom controls.

--- src/components/VisualComposerColorCustomiser/BottomControls/BottomControls.js
+++ src/components/VisualComposerColorCustomiser/BottomControls/BottomControls.js
@@ -17,7 +17,7 @@ const bottomControls = (props) => {
     return (
         <div style={controlDivStyle} className={`${styles.rootElement}`}>
             <div style={controlElementStyle} className={`d-inline-flex justify-content-center align-items-center ${styles.individualControlElementsStyle}`}>
-                Color
+                <input id="color-select" type="color" className={`p-0 m-0`}/>
             </div>
             <div style={controlElementStyle} className={`d-inline-flex justify-content-center align-items-center ${styles.individualControlElementsStyle}`}>
                 Reset

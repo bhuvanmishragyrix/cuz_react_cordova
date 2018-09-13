5dd7d7f9a7555f73fde9281b40e1cf0bf2033e21
Bhuvan Mishra <bhuvan.mishra@gyrix.co>
2018-09-13 19:27:51 +0530

    Added bottom controls to the payment page.

--- src/components/Payments/BottomControls/BottomControls.js
+++ src/components/Payments/BottomControls/BottomControls.js
@@ -6,6 +6,13 @@ const bottomControls = () => {
     return (
         <div>
             <div className={`text-center`}>
+                <div className={`d-flex justify-content-center align-items-center`}>
+                    <i className={`fa fa-shopping-cart ${styles.cartIcon} d-inline-block p-0 m-0`} aria-hidden="true"></i>
+                    <p className={`${styles.totalText} d-inline-block p-0 my-0 mx-1`}>Total</p>
+                    <i className={`fa fa-eur ${styles.euroIcon} d-inline-block p-0 ml-0 my-0 mr-1`} aria-hidden="true"></i>
+                    <p className={`${styles.priceText} d-inline-block p-0 my-0 mx-1`}>99.99</p>
+                </div>
+                <i className={`fa fa-check-circle-o ${styles.checkoutIcon}`} aria-hidden="true"></i>
                 <p className={`${styles.text}`}>CHECKOUT</p>
             </div>
         </div>

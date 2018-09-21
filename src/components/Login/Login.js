863cb010143ed3bd81c644a0c3596aa11f91a02d
Bhuvan Mishra <bhuvan.mishra@gyrix.co>
2018-09-21 16:12:28 +0530

    Added the login page skeleton.

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import styles from './Login.css';
 
 const login = () => {
     return (
-        <div>Hello World</div>
+        <div className={`mx-3`}>
+            <p className={`${styles.loginHeader} text-center`}>Login</p>
+            <label className={`${styles.labelText}`}>Email Id</label>
+            <input className={`form-control`} />
+            <label className={`${styles.labelText} mt-2`}>Password</label>
+            <input className={`form-control`} />
+            <div className={`text-center mt-2`}>
+                <button className={`btn`}>Login</button>
+            </div>
+        </div>
     );
 };
 

eead7283f4315d58ffc5b6fed5dfd765e8890f2d
Bhuvan Mishra <bhuvan.mishra@gyrix.co>
2018-09-21 17:12:05 +0530

    Sign up button added on login page.

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,15 +3,21 @@ import styles from './Login.css';
 
 const login = () => {
     return (
-        <div className={`mx-3`}>
+        <div className={`mx-3 mb-2`}>
             <p className={`${styles.loginHeader} text-center`}>Login</p>
             <label className={`${styles.labelText}`}>Email Id</label>
             <input className={`form-control`} />
             <label className={`${styles.labelText} mt-2`}>Password</label>
             <input className={`form-control`} />
-            <div className={`text-center mt-2`}>
-                <button className={`btn`}>Login</button>
+            <div className={`text-center mt-4`}>
+                <button className={`btn btn-primary form-control`}>Login</button>
             </div>
+            <div className={`d-flex justify-content-between align-items-center mt-4`}>
+                <div className={`d-inline ${styles.lineDiv}`}></div>
+                <p className={`d-inline ${styles.newToCuzText} m-0 p-0`}>New to CUZ?</p>
+                <div className={`d-inline ${styles.lineDiv}`}></div>
+            </div>
+            <button className={`btn btn-primary form-control mt-2`}>Sign up</button>
         </div>
     );
 };

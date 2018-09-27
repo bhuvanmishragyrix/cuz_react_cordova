6b92f52deac94d32c3951135e62c1fe5d8603798
Bhuvan Mishra <bhuvan.mishra@gyrix.co>
2018-09-27 15:18:59 +0530

    Added getSVGImageFromS3 function. Setting AWS credentials encapsulated into its own function.

--- src/util/AWSServicesManagement.js
+++ src/util/AWSServicesManagement.js
@@ -4,19 +4,38 @@ import * as appConstants from '../constants/AppConstants';
 
 AWS.config.region = appConstants.REGION;
 
-export function storeImageInS3(jWTToken) {
-
+const setAWSCredentials = (jWTToken) => {
     let loginAttributeNameToBeSetDynamically = {
     };
 
     loginAttributeNameToBeSetDynamically[`cognito-idp.${appConstants.REGION}.amazonaws.com/${appConstants.USER_POOL_ID}`] = jWTToken;
 
+
+    AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+        IdentityPoolId: appConstants.IDENTITY_POOL_ID,
+        Logins: loginAttributeNameToBeSetDynamically
+    });
+};
+
+export function storeImageInS3(jWTToken) {
+
     const promiseForImageStore = new Promise((resolve, reject) => {
-        AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-            IdentityPoolId: appConstants.IDENTITY_POOL_ID,
-            Logins: loginAttributeNameToBeSetDynamically
-        });
+
+        setAWSCredentials(jWTToken);
+
     });
 
     return promiseForImageStore;
+}
+
+export function getSVGImageFromS3(jWTToken) {
+
+    const promiseForImageStore = new Promise((resolve, reject) => {
+
+        setAWSCredentials(jWTToken);
+
+    });
+
+    return promiseForImageStore;
+
 }

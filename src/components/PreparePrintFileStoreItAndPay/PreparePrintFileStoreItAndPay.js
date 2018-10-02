8334f7b89c745588b68b402a4dcae8eba986e4da
Bhuvan Mishra <bhuvan.mishra@gyrix.co>
2018-10-02 10:59:00 +0530

    Added loader code to PreparePrintFileStoreItAndPay.js

--- src/components/PreparePrintFileStoreItAndPay/PreparePrintFileStoreItAndPay.js
+++ src/components/PreparePrintFileStoreItAndPay/PreparePrintFileStoreItAndPay.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import * as appConstants from '../../constants/AppConstants';
+import * as util from '../../util/Util';
 
 class PreparePrintFileStoreItAndPay extends Component {
 
@@ -8,10 +9,19 @@ class PreparePrintFileStoreItAndPay extends Component {
         marginTop: `${appConstants.HEIGHT_OF_THREE_ELEMENT_TAB_BAR + appConstants.TOP_MARGIN_FOR_THREE_ELEMENT_TABBAR_PAGES}px`
     }
 
+    constructor(props) {
+
+        super(props);
+
+        this.state = {
+            content: util.circularProgress()
+        };
+    }
+
     render() {
         return (
             <div style={this.topMargin}>
-                Hello World
+                {this.state.content}
             </div>
         );
     }

88fb31e3f1cdc391aed58183cc3c59bf82ef6ded
Bhuvan Mishra <bhuvan.mishra@gyrix.co>
2018-10-16 08:52:32 +0530

    Added Documentation for VisualComposerColorCustomiserPage.js

--- src/containers/VisualComposerColorCustomiserPage.js
+++ src/containers/VisualComposerColorCustomiserPage.js
@@ -3,7 +3,8 @@ import VisualComposerColorCustomiser from '../components/VisualComposerColorCust
 
 /**
  * <ul style="list-style:none;">
- * 
+ * <li> This is Visual Composer Color Customiser page.  </li>
+ * <li> It is a container for the VisualComposerColorCustomiser component (from src/components/VisualComposerColorCustomiser/VisualComposerColorCustomiser.js) </li>
  * </ul>
  */
 const visualComposerColorCustomiserPage = () => {

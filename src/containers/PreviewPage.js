d316d22664577e0136b865b015ac423383d2c58c
Bhuvan Mishra <bhuvan.mishra@gyrix.co>
2018-10-16 15:28:47 +0530

    Edited documentation of PreviewPage.js

--- src/containers/PreviewPage.js
+++ src/containers/PreviewPage.js
@@ -3,14 +3,16 @@ import Preview from '../components/Preview/Preview';
 
 /**
  * This the preview page.
- * <ul style="list-style:none;">
- * <li> Functional Description: </li>
+ *      <ul style="list-style:none;">
+ *      <li> Functional Description: </li>
  *      <ul>
- *          <li> In this page we can view our customisation as they would appear on the product (Dirt Bike), from the right side. </li>
+ *          <li> This is the just a container for the Preview component (from src/components/Preview/Preview.js) </li>
+ *          <li> Overall on the Preview page we can view our customisation as they would appear on the product (Dirt Bike), from the left and right side. </li>
+ *          <li> Currently only the right side image is available.</li>
  *      </ul>
- * <li> Code Description: </li>
+ *      <li> Code Description: </li>
  *      <ul>
- *          <li> This is the container for the Preview component (from src/components/Preview/Preview.js) </li>
+ *          <li> The Preview component (from src/components/Preview/Preview.js) is rendered here </li>
  *      </ul>
  * </ul>
  */

787865a0e182be728876f409755c53a07c104974
Bhuvan Mishra <bhuvan.mishra@gyrix.co>
2018-10-17 14:17:21 +0530

    Added documentation to CategorySelectPage.js

--- src/containers/CategorySelectPage.js
+++ src/containers/CategorySelectPage.js
@@ -3,6 +3,23 @@ import { connect } from 'react-redux';
 
 import CategorySelect from '../components/CategorySelect/CategorySelect';
 
+/**
+ * <ul style="list-style: none;">
+ *      <li> This is the Category Selection and New Products display Page. </li>
+ *      <li> Functional Description:</li>
+ *      <ul>
+ *          <li> This page is a container for CategorySelect component, which resides in src/components/CategorySelect/CategorySelect.js </li>
+ *          <li> Overall in the Category Select Page, the user can view all the available categories, and </li>
+ *          <li> View all the New Products, and  </li>
+ *          <li> Change the selected category from the category slider, and </li>
+ *          <li> Navigate forward by pressing the "Continue" button. </li>
+ *      </ul>
+ *      <li> Navigation Description: </li>
+ *      <ul>
+ *          <li> If the "Continue" button is enabled and we tap on it, we are redirected to route '/parentForThreeElementTabBarScreens/brandYearModalSelectPage'.</li>
+ *      </ul>
+ * </ul>
+ */
 class CategorySelectPage extends Component {
 
     constructor(props) {
